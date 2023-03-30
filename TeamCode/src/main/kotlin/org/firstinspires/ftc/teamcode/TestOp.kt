5e97a140b5272eff1f01991b1f454918d80c71a0
SciDev5 <skhuber0@gmail.com>
2023-03-30 15:53:28 -0400

    get git inited loser

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/TestOp.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp
+class TestOp : OpMode() {
+    override fun init() {
+    }
+
+    override fun loop() {
+
+    }
+
+}
